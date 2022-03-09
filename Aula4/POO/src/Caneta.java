0bd08d09b040fa4794b460997fc219fc29136b44
Ettore <ettore.dalben@gmail.com>
2022-03-09 11:09:29 -0300

    criando classes com java

---
+++ Aula4/POO/src/Caneta.java
@@ -0,0 +1,11 @@
+public class Caneta {
+    // Caracteristicas da caneta
+    String modelo, cor;
+    double ponta;
+    int carga;
+
+    // Comportamentos das canetas
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+}
