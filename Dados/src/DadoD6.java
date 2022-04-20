3cb1a6892af46efcf51d168c61a6594c378bde65
EttoreK <ettore.dalben@gmail.com>
2022-04-20 10:49:50 -0300

    Exemplo de Herenca com dado

---
+++ Dados/src/DadoD6.java
@@ -0,0 +1,5 @@
+public class DadoD6 extends Dado{
+    public DadoD6(String id){
+        super(id);
+    }
+}
---
+++ Dados/src/TestDrive.java
@@ -0,0 +1,31 @@
+public class TestDrive{
+    public static void executar(){
+        Dado d1 = new Dado("1234");
+        System.out.println("Criado Dado Generico: " + d1);
+        System.out.println("Face atual: " + d1.faceAtual());
+        //Sortear nova Face
+        d1.rodar();
+        System.out.println("Face atual: " + d1.faceAtual());
+
+        Dado d2 = new DadoD6("3473");
+        System.out.println("Criado Dado D6: " + d2);
+        System.out.println("Face atual: " + d2.faceAtual());
+        //Sortear nova Face
+        d2.rodar();
+        System.out.println("Face atual: " + d2.faceAtual());
+
+        Dado d3 = new DadoD10("4358");
+        System.out.println("Criado Dado D10: " + d3);
+        System.out.println("Face atual: " + d3.faceAtual());
+        //Sortear nova Face
+        d3.rodar();
+        System.out.println("Face atual: " + d3.faceAtual());
+
+        Dado d4 = new DadoD20("2391");
+        System.out.println("Criado Dado D20: " + d4);
+        System.out.println("Face atual: " + d4.faceAtual());
+        //Sortear nova Face
+        d4.rodar();
+        System.out.println("Face atual: " + d4.faceAtual());
+    }
+}
