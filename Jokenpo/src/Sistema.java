b7db4f73fb5813b2cac3a3b689b6e0e500a7802a
EttoreK <ettore.dalben@gmail.com>
2022-05-05 12:35:01 -0300

    SPOCK

--- Jokenpo/src/Sistema.java
+++ Jokenpo/src/Sistema.java
@@ -13,20 +13,26 @@ public class Sistema{
 	}
 
 	private static Jogada sortearJogada(){
-		Jogada jogadas[] = new Jogada[3];
+		Jogada jogadas[] = new Jogada[5];
 		jogadas[0] = new Pedra();
 		jogadas[1] = new Papel();
 		jogadas[2] = new Tesoura();
+		jogadas[3] = new Spock();
+		jogadas[4] = new Lagarto();
 		return jogadas[ThreadLocalRandom.current().nextInt(jogadas.length)];
 	}
 
 	private static String avaliaJogadas(Jogada j1, Jogada j2){
+		System.out.println(j1.toString());
+		System.out.println(j2.toString());
 		if(j1.verificarVenceu(j2)){
 			return "Jogada 1";
 		}
-		if(j2.verificarVenceu(j1)){
+		else if(j2.verificarVenceu(j1)){
 			return "Jogada 2";
 		}
-		return "Empate";
+		else{
+			return "Empate";
+		}
 	}
 }
