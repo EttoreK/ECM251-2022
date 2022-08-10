8b80d9e9fc35c509146e46b43f7aa18026f0fe07
EttoreK <ettore.dalben@gmail.com>
2022-08-10 11:18:45 -0300

    Revisão de python

---
+++ py1 - revisão/Input.py
@@ -0,0 +1,9 @@
+# msg = 'informe seu nome'
+# primeiroNome = input()
+# segundoNome = input()
+# print(f'Seu Nome é: {primeiroNome} {segundoNome}')
+
+n1 = int(input())
+n2 = float(input())
+res = n1 + n2
+print (res)
