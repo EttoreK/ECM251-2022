94b13235244593aac6e08b48808febf0d1a5cd5d
EttoreK <ettore.dalben@gmail.com>
2022-08-17 11:07:07 -0300

    Aula16

---
+++ Python/Aula16/main.py
@@ -0,0 +1,17 @@
+from Modelos.documentos import Book, Document, EMail
+from Modelos.plantas import Arvore, Alface
+
+def run_system():
+	doc1 = Document()
+	doc2 = EMail(to='email@sim', authors=["Eu"])
+	doc3 = Book(title="Design", authors=["Eric", "John", "Ralph", "Ricardão"])
+	print(doc2.get_authors())
+	print(doc3)
+
+if __name__ == "__main__":
+	# planta1 = Arvore('Carvalho')
+	# planta2 = Alface(nomes="Hamburguer")
+
+	# print(planta1.ola())
+	# print(planta2.ola())
+	run_system()
