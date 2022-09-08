51c96e17312fcdb77b11bedbeda0c43a993b34ad
ETTORE PADULA DALBEN <20.00387-0@maua.br>
2022-09-08 12:54:00 -0300

    comecei a fzr o login

--- Python/Projetolabloja/home.py
+++ Python/Projetolabloja/home.py
@@ -1,4 +1,6 @@
 from cgitb import text
+from email.mime import base
+from sre_constants import JUMP
 from tkinter.ttk import Style
 from turtle import color
 from unittest.mock import DEFAULT
@@ -15,9 +17,61 @@ class MinhaUI():
             maxsize= (1800,900),
             alpha=1.0,
             themename='superhero',
-            iconphoto='calculator.png'
+            iconphoto='games.png'
         )
         return janela
+
+    def _criar_texto(self, txt):
+        return ttk.Entry(
+            self.base,
+            textvariable=txt,
+        )
+    
+    def _entrar_usuario(self, usuario):
+        usuario = self._criar_texto(usuario)
+        return usuario
+
+    def _entrar_senha(self, senha):
+        senha = self._criar_texto(senha)
+        return senha
+
+
+    # # store email address and password
+    # email = "34"
+    # password = "3434"
+
+    # def login_clicked(email, password):
+    #     """ callback when the login button clicked
+    #     """
+    #     msg = f'You entered email: {email} and password: {password}'
+    #     # showinfo(
+    #     #     title='Information',
+    #     #     message=msg
+    #     # )
+
+    #     # Sign in frame
+    #     signin = ttk.Frame(base)
+    #     signin.pack(padx=10, pady=10, fill='x', expand=True)
+
+
+    #     # email
+    #     email_label = ttk.Label(signin, text="Email Address:")
+    #     email_label.pack(fill='x', expand=True)
+
+    #     email_entry = ttk.Entry(signin, textvariable=email)
+    #     email_entry.pack(fill='x', expand=True)
+    #     email_entry.focus()
+
+    #     # password
+    #     password_label = ttk.Label(signin, text="Password:")
+    #     password_label.pack(fill='x', expand=True)
+
+    #     password_entry = ttk.Entry(signin, textvariable=password, show="*")
+    #     password_entry.pack(fill='x', expand=True)
+
+    #     # login button
+    #     # login_button = ttk.Button(signin, text="Login", command=login_clicked)
+    #     # login_button.pack(fill='x', expand=True, pady=10)
 
     def _criar_botao(self, texto, acao):
         return ttk.Button(
@@ -27,25 +81,35 @@ class MinhaUI():
             command=acao
         )
 
-    def _criar_texto(self, guardar):
-        return ttk.Entry(
-            self.base,
-            textvariable=guardar
-        )
-
     def __init__(self) -> None:
         self.base = self._construir_base()
         self.bot = self._criar_botao(texto="AÇÃO", acao=self.mostrar_texto)
         self.bot.grid(row=3, column=2, padx=5, pady=5)
         self.valor_digitado = ""
-        self.text = self._criar_texto(guardar=self.valor_digitado)
-        self.text.grid(row=2, column=1, padx=5, pady=5)
+        # self.text = self._criar_texto(usuario=self.valor_digitado)
+        # self.text = self.login_clicked( password="123")
+        self.usuario = self._entrar_usuario(usuario='')
+        self.usuario.grid(row=2, column=1, padx=5, pady=5)
     
     def run(self):
         self.base.mainloop()
 
     def mostrar_texto(self):
-        print("Valor Digitado:", self.text.get())
+        user = self.usuario.get()
+        with open('usuarios.txt') as f:
+            lines = f.readlines()
+        for a in lines:
+            print(user)
+            print(a)
+            if (a=='\n'):
+                break
+            elif (a == user):
+                print("Entrou: ", user)
+                break
+            elif (a == ''):
+                print("Falha: ")
+                break
+        
 
 
 if __name__ == "__main__":
